31c3b2ac1c4322fac4df568ec6745f41351d5b4c
Stuart George <stuart@accentdesign.co.uk>
2019-04-29 22:59:26 +0100

    added __all__

--- starlette_auth/__init__.py
+++ starlette_auth/__init__.py
@@ -1,8 +1,13 @@
 __version__ = "0.0.1.b1"
 
 
-from starlette_auth.backends import *
-from starlette_auth.config import *
-from starlette_auth.main import *
-from starlette_auth.schemas import *
-from starlette_auth.tables import *
+from starlette_auth.backends import ModelAuthBackend
+from starlette_auth.config import config
+from starlette_auth.main import app
+
+
+__all__ = [
+    'ModelAuthBackend',
+    'config',
+    'app'
+]
