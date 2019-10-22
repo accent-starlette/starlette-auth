d05e2f4e2e5ca65d25bd65696af1c1c35049788f
Stuart George <stuart@accentdesign.co.uk>
2019-10-22 11:57:56 +0100

    added an exception to catch unset templates in email

--- starlette_auth/forms.py
+++ starlette_auth/forms.py
@@ -5,6 +5,7 @@ from starlette_core.mail import send_message
 from wtforms import fields, form, validators
 from wtforms.fields.html5 import EmailField
 
+from .exceptions import ImproperlyConfigured
 from .tables import User
 from .tokens import token_generator
 from .utils.http import urlsafe_base64_encode
@@ -55,6 +56,18 @@ class PasswordResetForm(form.Form):
             "token": token_generator.make_token(user),
         }
         msg = EmailMessage()
+
+        if (
+            not config.reset_pw_email_subject_template
+            or not config.reset_pw_email_template
+        ):
+            error_message = (
+                "To sent a password reset email you must specify both the "
+                "`reset_pw_email_subject_template` and `reset_pw_email_template` "
+                "templates. Additionally you can also specify the "
+                "`reset_pw_html_email_template` to send an html version."
+            )
+            raise ImproperlyConfigured(error_message)
 
         subject_tmpl = templates.get_template(config.reset_pw_email_subject_template)
         subject = subject_tmpl.render(context)
